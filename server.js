82b0964d4ff4f6d3ca7bc4f6f37c7b32f754c110
Lucas-DeReamer <lu515886@ucf.edu>
2025-02-20 13:27:24 -0500

    console log

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 const dotenv = require('dotenv').config();
-app.use(dotenv());
 app.use(bodyParser.json());
 app.use(express.json());
 
