82e445d475ed7c3d323c408a1c14e70754171df1
Lucas-DeReamer <lu515886@ucf.edu>
2025-03-04 12:29:46 -0500

    Add sid count to db

--- server.js
+++ server.js
@@ -103,9 +103,9 @@ app.post("/submit", async (req, res, next) => {
             //sidquery.limit(1);
             //const currentH = sidquery.exec();
             await PKRec.find({}).sort({ sid: -1 }).limit(1).exec(function (err, model) {
-                const currentH = model;
+                console.log(model);
             });
-            console.log(currentH);
+            //console.log(currentH);
             //const curSid = currentH.sid;
             console.log("got past cursid assign");
 
