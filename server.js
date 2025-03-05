a006c977770c65b4117d5b8b5db33524b9e2a816
Lucas-DeReamer <lu515886@ucf.edu>
2025-03-04 20:25:22 -0500

    Add get keys

--- server.js
+++ server.js
@@ -143,7 +143,7 @@ app.get("/getkeys", async (req, res, next) => {
             });
         } else {
 
-            const allKeys = await PKRec.find({}, { PK: 1});
+            const allKeys = await PKRec.find({}, { _id: 0, name: 0, __v: 0});
 
             // Return a single JSON response ------User Del
             res.status(200).json(allKeys);
