f7c8d6dfa15483f0934fa3f6ebd2cf409c6737de
Lucas-DeReamer <lu515886@ucf.edu>
2025-03-12 12:09:34 -0400

    Update form response to give info

--- client/src/Message.tsx
+++ client/src/Message.tsx
@@ -4,7 +4,8 @@ function Message() {
     //jsx
     return (<><h1>Enter your WG Public Key</h1>
         <p>
-            <a href="https://docs.google.com/document/d/1Am6y9t0juq5GxyVMUJn6ef7ds-3ldGiAjIqQtAun1wc">Instructions link</a>
+            <a href="https://docs.google.com/document/d/1Am6y9t0juq5GxyVMUJn6ef7ds-3ldGiAjIqQtAun1wc"
+                target="_blank">Instructions link</a>
         </p></>);
 
 }
